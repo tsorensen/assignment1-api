1f40ed5d6ca3b4a24c5f69a7ce0133cb575efc51
tsorensen <tysonsorensen3@gmail.com>
2016-02-08 14:43:36 -0700

    adds all functions for part 1

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 var reps = require('./lib/reps');
 
-reps.allByZip('84043', function(err, results) {
+reps.allByZip('84606', function(err, results) {
+  if(err) {
+    return console.log(err);
+  }
+  console.log(results);
+});
+
+reps.repsByName('Smith', function(err, results) {
+  if(err) {
+    return console.log(err);
+  }
+  console.log(results);
+});
+
+reps.repsByState('UT', function(err, results) {
+  if(err) {
+    return console.log(err);
+  }
+  console.log(results);
+});
+
+reps.sensByName('Hatch', function(err, results) {
+  if(err) {
+    return console.log(err);
+  }
+  console.log(results);
+});
+
+reps.sensByState('UT', function(err, results) {
   if(err) {
     return console.log(err);
   }
