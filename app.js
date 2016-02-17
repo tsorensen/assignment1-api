330aae5563c7137a8cc7b277c986c4e180e9c821
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-02-17 16:48:03 -0700

    adds templating for part 3

--- app.js
+++ app.js
@@ -29,49 +29,16 @@ app.get('/', function(req, res, next) {
         reps: people,
       });
     });
+  } else {
+    res.render('index');
   }
 });
 
-// app.get('/all/by-zip/:zip', function(req, res, next) {
-//   reps.allByZip(req.params.zip, function(err, people) {
-//     if(err) { return next(err); }
-//     res.json(people);
-//   });
-// });
-//
-// app.get('/reps/by-name/:name', function(req, res, next) {
-//   reps.repsByName(req.params.name, function(err, people) {
-//     if(err) { next(err); }
-//     res.json(people);
-//   });
-// });
-//
-// app.get('/reps/by-state/:state', function(req, res, next) {
-//   reps.repsByState(req.params.state, function(err, people) {
-//     if(err) { next(err); }
-//     res.json(people);
-//   });
-// });
-//
-// app.get('/sens/by-name/:name', function(req, res, next) {
-//   reps.sensByName(req.params.name, function(err, people) {
-//     if(err) { next(err); }
-//     res.json(people);
-//   });
-// });
-//
-// app.get('/sens/by-state/:state', function(req, res, next) {
-//   reps.sensByState(req.params.state, function(err, people) {
-//     if(err) { next(err); }
-//     res.json(people);
-//   });
-// });
-
 //error handling
-app.use(function(err, req, res, next) {
-  res.status(500);
-  res.render('error', { error: err });
-});
+// app.use(function(err, req, res, next) {
+//   res.status(500);
+//   res.render('error', { error: err });
+// });
 
 app.listen(PORT, function() {
   console.log(`Server listening on port ${PORT}`);
