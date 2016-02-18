dcdf8b27992d8f06842dc88ee851077cfa777e92
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-02-18 09:08:38 -0700

    adds some jade stuff for the returned query, adds back in all api endpoints

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('view cache', false);
 
 app.get('/', function(req, res, next) {
   var method;
+
   if(req.query.group === 'all') {
     method = reps.allByZip;
   } else if(req.query.group === 'reps' && req.query.type === 'name') {
@@ -24,25 +25,61 @@ app.get('/', function(req, res, next) {
     method = reps.sensByName;
   } else if(req.query.group === 'sens' && req.query.type === 'state') {
     method = reps.sensByState;
+  } else {
+    method = function(param, callback) { callback(null, []); };
   }
 
-  if(method) {
-    method(req.query.search, function(err, people) {
-      if(err) { return next(err); }
-      res.render('index', {
-        reps: people,
-      });
+  method(req.query.search, function(err, people) {
+    if(err) { return next(err); }
+    res.render('index', {
+      reps: people,
+      query: req.query,
     });
-  } else {
-    res.render('index');
-  }
+  });
+
+});
+
+
+app.get('/all/by-zip/:zip', function(req, res, next) {
+  reps.allByZip(req.params.zip, function(err, people) {
+    if(err) { next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-name/:name', function(req, res, next) {
+  reps.repsByName(req.params.name, function(err, people) {
+    if(err) { next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-state/:state', function(req, res, next) {
+  reps.repsByState(req.params.state, function(err, people) {
+    if(err) { next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-name/:name', function(req, res, next) {
+  reps.sensByName(req.params.name, function(err, people) {
+    if(err) { next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-state/:state', function(req, res, next) {
+  reps.sensByState(req.params.state, function(err, people) {
+    if(err) { next(err); }
+    res.json(people);
+  });
 });
 
 //error handling
-// app.use(function(err, req, res, next) {
-//   res.status(500);
-//   res.render('error', { error: err });
-// });
+app.use(function(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+});
 
 app.listen(PORT, function() {
   console.log(`Server listening on port ${PORT}`);
